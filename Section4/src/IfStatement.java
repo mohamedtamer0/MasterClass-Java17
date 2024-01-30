89404baa9242726ef9c1fe3e5582cab9cdc690e3
mohamedtamer0 <mt4452242@gmail.com>
2024-01-30 20:45:16 +0200

    Section4

---
+++ Section4/src/IfStatement.java
@@ -0,0 +1,8 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien !");
+        }
+    }
+}
