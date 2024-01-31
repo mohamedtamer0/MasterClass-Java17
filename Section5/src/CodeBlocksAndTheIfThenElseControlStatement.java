8ed6c07c64c5bb5aaf3b169e1c0138ffe9f50b2c
mohamedtamer0 <mt4452242@gmail.com>
2024-01-31 22:07:43 +0200

    Section5

---
+++ Section5/src/CodeBlocksAndTheIfThenElseControlStatement.java
@@ -0,0 +1,17 @@
+public class CodeBlocksAndTheIfThenElseControlStatement {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your Score Was Less Than 500 but grater than 1000");
+        } else if (score < 1000) {
+            System.out.println("Your Score Was Less than 1000");
+        } else {
+            System.out.println("Got here");
+        }
+    }
+
+}
