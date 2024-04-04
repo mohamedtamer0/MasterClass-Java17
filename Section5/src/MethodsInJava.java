995d4bb6e751826e79389afc33195233adf71967
Mohamed Tamer <mohamed.tamer@elsewedy.com>
2024-04-04 11:31:41 +0200

    update

--- Section5/src/MethodsInJava.java
+++ Section5/src/MethodsInJava.java
@@ -1,7 +1,16 @@
 public class MethodsInJava {
     public static void main(String[] args) {
+
+        boolean newGameOver = true;
+        int newScore = 10000;
+        int newLevelCompleted = 8;
+        int newBonus = 200;
+
+
         calculateScore();
         calculateScoreTwo(true, 20000, 9, 300);
+        calculateScoreTwo(newGameOver,newScore,newLevelCompleted,newBonus);
+
     }
 
     public static void calculateScore() {
