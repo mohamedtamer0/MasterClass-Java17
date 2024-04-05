26d6fa06a054a86e560c020698be4cd4e1ef1f0b
mohamed.tamer0 <mt4452242@gmail.com>
2024-04-05 21:00:10 +0200

    update

---
+++ Section5/src/MethodOverloading.java
@@ -0,0 +1,20 @@
+public class MethodOverloading {
+    public static void main(String[] args) {
+        int newScore = calcScore("Tamer", 500);
+        System.out.println("New Score Is " + newScore);
+
+        calcScore(75);
+    }
+
+    public static int calcScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " Scored " + score + " points");
+        return score * 1000;
+    }
+
+
+    public static int calcScore(int score) {
+        System.out.println("Unnamed Player Scored " + score + " points");
+        return score * 1000;
+    }
+
+}
