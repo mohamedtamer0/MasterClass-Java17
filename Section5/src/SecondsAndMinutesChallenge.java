1ca6a2abaa6f0d07e3756ea49611808484e6d302
mohamed.tamer0 <mt4452242@gmail.com>
2024-04-06 11:24:25 +0200

    update

--- Section5/src/SecondsAndMinutesChallenge.java
+++ Section5/src/SecondsAndMinutesChallenge.java
@@ -1,11 +1,20 @@
 public class SecondsAndMinutesChallenge {
     public static void main(String[] args) {
-        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
         System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945));
     }
 
     private static String getDurationString(int seconds) {
         //Two step approach to get hours
+
+        if (seconds < 0) {
+            return "Invalid data for seconds(" + seconds
+                    + "), must be a positive integer value";
+        }
+
         int minutes = seconds / 60;
         int hours = minutes / 60;
         System.out.println("hours = " + hours);
@@ -17,6 +26,17 @@ public class SecondsAndMinutesChallenge {
     }
 
     private static String getDurationString(int minutes, int seconds) {
+
+        if (minutes < 0) {
+            return "Invalid data for minutes(" + minutes
+                    + "), must be a positive integer value";
+        }
+
+        if (seconds <= 0 || seconds >= 0) {
+            return "Invalid data for seconds(" + minutes
+                    + "), must be between 0 and 59";
+        }
+
         int hours = minutes / 60;
 
         int remainingMinutes = minutes % 60;
