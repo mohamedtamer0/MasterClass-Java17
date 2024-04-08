9b501c0bb79e5fb6c321cfc110c5b846bbf8dad0
mohamedtamer0 <mt4452242@gmail.com>
2024-04-08 17:55:25 +0200

    update

---
+++ Section6-ControlFlow/src/main/java/org/example/SwitchStatement.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+        int switchValue = 3;
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("was not 1 or 2");
+                break;
+        }
+    }
+}
