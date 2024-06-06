1fd235163ccb4ecf2e889d3ae4ded2f9f6f4ac03
mohamedtamer0 <mt4452242@gmail.com>
2024-06-06 20:04:34 +0300

    update

--- Section6-ControlFlow/src/main/java/org/example/TheWhileAndDoWhileStatements.java
+++ Section6-ControlFlow/src/main/java/org/example/TheWhileAndDoWhileStatements.java
@@ -13,5 +13,26 @@ public class TheWhileAndDoWhileStatements {
             System.out.println(j);
             j++;
         }
+
+
+        int x = 1;
+        boolean isReadyx = true;
+        do {
+            if (x > 5) break;
+            System.out.println(x);
+            x++;
+            isReadyx = (x > 0);
+        } while (isReadyx);
+
+
+        int number = 0;
+        while (number < 50) {
+            number += 5;
+            if (number % 25 == 0) {
+                continue;
+            }
+            System.out.print(number + "_");
+        }
+
     }
 }
