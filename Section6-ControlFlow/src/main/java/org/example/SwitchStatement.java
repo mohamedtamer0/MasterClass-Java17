c6cf2624f6db4c0c96742241eac295a746506ebf
mohamedtamer0 <mt4452242@gmail.com>
2024-06-06 17:10:48 +0300

    update

--- Section6-ControlFlow/src/main/java/org/example/SwitchStatement.java
+++ Section6-ControlFlow/src/main/java/org/example/SwitchStatement.java
@@ -2,13 +2,16 @@ package org.example;
 
 public class SwitchStatement {
     public static void main(String[] args) {
-        int switchValue = 2;
+        int switchValue = 3;
         switch (switchValue) {
             case 1:
                 System.out.println("Value was 1");
                 break;
             case 2:
-                System.out.println("Value was 2");
+            case 3:
+            case 4:
+                System.out.println("Was a 2,3,4");
+                System.out.println("Actually it was a " + switchValue);
                 break;
             default:
                 System.out.println("was not 1 or 2");
