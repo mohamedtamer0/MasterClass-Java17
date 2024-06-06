da10a89a19a468dca32b867b968090bb1b0c55b9
mohamedtamer0 <mt4452242@gmail.com>
2024-06-06 18:39:42 +0300

    update

---
+++ Section6-ControlFlow/src/main/java/org/example/TheForStatement.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class TheForStatement {
+    public static void main(String[] args) {
+        for (int counter = 1; counter <= 5; counter++) {
+            System.out.println(counter);
+        }
+
+        for (double rate = 2.0; rate <= 5.0; rate++) {
+            double interestAmount = calculateInterest(10000.0, rate);
+            System.out.println("10,000 at " + rate + "% interest = " + interestAmount);
+        }
+
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+}
