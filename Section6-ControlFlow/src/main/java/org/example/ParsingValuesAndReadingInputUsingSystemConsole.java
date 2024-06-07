ceb625e4c5decb2bf458caaa1d8e0bee89011a66
mohamedtamer0 <mt4452242@gmail.com>
2024-06-07 14:10:08 +0300

    update

--- Section6-ControlFlow/src/main/java/org/example/ParsingValuesAndReadingInputUsingSystemConsole.java
+++ Section6-ControlFlow/src/main/java/org/example/ParsingValuesAndReadingInputUsingSystemConsole.java
@@ -39,9 +39,28 @@ public class ParsingValuesAndReadingInputUsingSystemConsole {
         System.out.println("Hi " + name + ", Thanks for taking the course!");
 
         System.out.println("What year were you born?");
-        String dateOfBirth = scanner.nextLine();
-        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        boolean validDOB = false;
+        int age = 0;
+        do {
+            System.out.println("Enter a year of birth >= " + (currentYear - 125) + " and <= " + (currentYear));
+            try {
+                age = checkData(currentYear, scanner.nextLine());
+                validDOB = age < 0 ? false : true;
+            } catch (NumberFormatException badUserData) {
+                System.out.println("Characters not allowed!!! Try again.");
+            }
+        } while (!validDOB);
 
         return "So you are " + age + " years old";
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+        int dob = Integer.parseInt(dateOfBirth);
+        int minYear = currentYear - 125;
+        if ((dob < minYear) || (dob > currentYear)) {
+            return -1;
+        }
+        return (currentYear - dob);
     }
 }
