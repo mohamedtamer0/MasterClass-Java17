1959627e1c180e84db75f2e3ddb7fa03dc9f9644
mohamedtamer0 <mt4452242@gmail.com>
2024-06-07 11:43:19 +0300

    update

---
+++ Section6-ControlFlow/src/main/java/org/example/TheWhileLoopChallengeExercise.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class TheWhileLoopChallengeExercise {
+    public static void main(String[] args) {
+        int number = 4;
+        int finishNumber = 20;
+        while (number <= finishNumber) {
+            number++;
+            if (!isEvenNumber(number)) {
+                continue;
+            }
+            System.out.println("Even Number " + number);
+        }
+    }
+
+    public static boolean isEvenNumber(int number) {
+        if ((number % 2) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
