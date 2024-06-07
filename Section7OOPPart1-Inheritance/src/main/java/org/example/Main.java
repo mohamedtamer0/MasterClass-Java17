58ca07d7734845d6265e4d566aeeceece721b90c
mohamedtamer0 <mt4452242@gmail.com>
2024-06-07 16:28:19 +0300

    update

--- Section7OOPPart1-Inheritance/src/main/java/org/example/Main.java
+++ Section7OOPPart1-Inheritance/src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
+        car.setMake("Porsche");
+        System.out.println("Make = " + car.getMake());
+        System.out.println("Model = " + car.getModel());
         car.describeCar();
     }
 }
