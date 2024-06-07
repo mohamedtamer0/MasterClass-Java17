2072a87c08e204be3327b56c0d9d2b8e67abfc8f
mohamedtamer0 <mt4452242@gmail.com>
2024-06-07 13:23:48 +0300

    update

--- Section6-ControlFlow/src/main/java/org/example/TheWhileLoopChallengeExercise.java
+++ Section6-ControlFlow/src/main/java/org/example/TheWhileLoopChallengeExercise.java
@@ -4,13 +4,19 @@ public class TheWhileLoopChallengeExercise {
     public static void main(String[] args) {
         int number = 4;
         int finishNumber = 20;
+        int evenCount = 0;
+        int oddCount = 0;
         while (number <= finishNumber) {
             number++;
             if (!isEvenNumber(number)) {
+                oddCount++;
                 continue;
             }
             System.out.println("Even Number " + number);
+            evenCount++;
         }
+        System.out.println("Total odd number found = " + oddCount);
+        System.out.println("Total even number found = " + evenCount);
     }
 
     public static boolean isEvenNumber(int number) {
