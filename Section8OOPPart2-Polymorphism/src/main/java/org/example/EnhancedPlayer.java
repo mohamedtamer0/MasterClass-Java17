93c0cd0477afe754035a3319bb6b4753836bb09a
mohamedtamer0 <mt4452242@gmail.com>
2024-06-19 16:45:55 +0300

    update

---
+++ Section8OOPPart2-Polymorphism/src/main/java/org/example/EnhancedPlayer.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class EnhancedPlayer {
+    private String fullName;
+    private int health;
+    private String weapon;
+
+    public EnhancedPlayer(String fullName) {
+        this(fullName, 100, "Sword");
+    }
+
+    public EnhancedPlayer(String fullName, int health, String weapon) {
+        this.fullName = fullName;
+        if (health <= 0) {
+            this.health = 1;
+        } else if (health > 100) {
+            this.health = 100;
+        } else {
+            this.health = health;
+        }
+        this.weapon = weapon;
+    }
+
+    public void loseHealth(int damage) {
+        health = health - damage;
+        if (health <= 0) {
+            System.out.println("Player knocked out of game");
+        }
+    }
+
+    public int healthRemaining() {
+        return health;
+    }
+
+    public void restoreHealth(int extraHealth) {
+        health = health + extraHealth;
+        if (health > 100) {
+            System.out.println("Player restored to 100%");
+            health = 100;
+        }
+    }
+
+    public static void main(String[] args) {
+        EnhancedPlayer alwqar = new EnhancedPlayer("Alwqar", 200, "Sword");
+        System.out.println("Initial health is " + alwqar.healthRemaining());
+    }
+}
+
