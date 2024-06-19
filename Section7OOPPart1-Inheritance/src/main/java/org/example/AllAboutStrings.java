02b7a8cbc016a4ce401e1808020285823965db5e
mohamedtamer0 <mt4452242@gmail.com>
2024-06-19 12:13:11 +0300

    update

--- Section7OOPPart1-Inheritance/src/main/java/org/example/AllAboutStrings.java
+++ Section7OOPPart1-Inheritance/src/main/java/org/example/AllAboutStrings.java
@@ -3,11 +3,27 @@ package org.example;
 public class AllAboutStrings {
     public static void main(String[] args) {
         printInfo("Hello World");
+        printInfo("");
+        printInfo("\t \n");
+
+        String helloWorld = "Hello World";
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('r'));
+        System.out.printf("index of World = %d %n", helloWorld.indexOf("World"));
     }
 
     public static void printInfo(String string) {
         int length = string.length();
         System.out.printf("Length = %d %n", length);
+
+        if (string.isEmpty()) {
+            System.out.println("String IS Empty");
+            return;
+        }
+
+        if (string.isEmpty()) {
+            System.out.println("String Is Blank");
+        }
+
         System.out.printf("First char = %c %n", string.charAt(0));
         System.out.printf("Last char = %c %n", string.charAt(length - 1));
     }
