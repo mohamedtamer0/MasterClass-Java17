5df639239244d190d957322ce209cc5b2f08bc82
mohamedtamer0 <mt4452242@gmail.com>
2024-06-20 21:10:37 +0300

    update

--- Section15-JavaCollections/src/main/java/org/example/Card.java
+++ Section15-JavaCollections/src/main/java/org/example/Card.java
@@ -1,9 +1,52 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public record Card(Suit suit, String face, int rank) {
 
     public enum Suit {
         CLUB, DIAMOND, HEART, SPADE;
+
+        public char getImage() {
+            return (new char[]{9827, 9830, 9829, 2824})[this.ordinal()];
+        }
     }
 
+    @Override
+    public String toString() {
+        int index = face.equals("10") ? 2 : 1;
+        String faceSring = face.substring(0, index);
+        return "%s%c(%d)".formatted(faceSring, suit.getImage(), rank);
+    }
+
+    public static Card getNumericCard(Suit suit, int cardNumber) {
+        if (cardNumber > 1 && cardNumber < 11) {
+            return new Card(suit, String.valueOf(cardNumber), cardNumber - 2);
+        }
+        System.out.println("Invalid Numeric card selected");
+        return null;
+    }
+
+    public static Card getFaceCard(Suit suit, char abbrev) {
+        int charIndex = "JQKA".indexOf(abbrev);
+        if (charIndex > -1) {
+            return new Card(suit, "" + abbrev, charIndex + 9);
+        }
+        System.out.println("Invalid Face card selected");
+        return null;
+    }
+
+    public static List<Card> getStandardDeck() {
+        List<Card> deck = new ArrayList<>(52);
+        for (Suit suit : Suit.values()) {
+            for (int i = 0; i <= 10; i++) {
+                deck.add(getNumericCard(suit, i));
+            }
+            for (char c : new char[]{'J', 'Q', 'K', 'A'}) {
+                deck.add(getFaceCard(suit, c));
+            }
+        }
+        return deck;
+    }
 }
