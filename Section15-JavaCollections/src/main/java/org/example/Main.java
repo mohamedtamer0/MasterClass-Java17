9b5e38be83138c0a826f03576d126cc0c7e85eb0
mohamedtamer0 <mt4452242@gmail.com>
2024-06-21 15:21:32 +0300

    update

--- Section15-JavaCollections/src/main/java/org/example/Main.java
+++ Section15-JavaCollections/src/main/java/org/example/Main.java
@@ -21,6 +21,20 @@ public class Main {
         List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
         Card.printDeck(acesOfHearts, "Aces Of Hearts", 1);
 
+        Card kingClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
+        List<Card> kingOfClubs = Collections.nCopies(13, kingClubs);
+        Card.printDeck(kingOfClubs, "Kings Of Clubs", 1);
+
+        Collections.addAll(cards, cardArray);
+        Collections.addAll(cards, cardArray);
+        Card.printDeck(cards, "Card Collection With Aces added", 2);
+
+        Collections.copy(cards, kingOfClubs);
+        Card.printDeck(cards, "Card Collection With Kings copied", 2);
+
+        cards = List.copyOf(kingOfClubs);
+        Card.printDeck(cards, "List Copy Of Kings", 1);
+
     }
 
 }
