69787db78cbab4a773608f6baede4a00356563e1
mohamedtamer0 <mt4452242@gmail.com>
2024-07-02 20:42:31 +0300

    update

--- Section10-List_ArrayList_LinkedList_Itrator_Autoboxing/src/main/java/org/example/LinkedListQS.java
+++ Section10-List_ArrayList_LinkedList_Itrator_Autoboxing/src/main/java/org/example/LinkedListQS.java
@@ -1,18 +1,21 @@
 package org.example;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class LinkedListQS {
     public static void main(String[] args) {
         var test = new LinkedList<String>();
         test.add("Osama");
-        test.add(0,"Elzero");
+        test.add(0, "Elzero");
 
         System.out.println(test);
 
 
         addMoreElements(test);
         System.out.println(test);
+
+        printItinerary(test);
     }
 
     private static void addMoreElements(LinkedList<String> list) {
@@ -26,5 +29,17 @@ public class LinkedListQS {
 
         //Stack
         list.push("Stack Push");
+    }
+
+    public static void printItinerary(LinkedList<String> list) {
+        System.out.println("Trip starts at " + list.getFirst());
+        String previousTown = list.getFirst();
+        ListIterator<String> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            var town = iterator.next();
+            System.out.println("--> From: " + previousTown + " to " + town);
+            previousTown = town;
+        }
+        System.out.println("Trip ends at " + list.getLast());
     }
 }
