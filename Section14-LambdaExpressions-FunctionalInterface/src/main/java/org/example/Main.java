094698b5caba039cdd8a7001bfcbb6f198f9779e
mohamedtamer0 <mt4452242@gmail.com>
2024-07-04 11:37:03 +0300

    update

--- Section14-LambdaExpressions-FunctionalInterface/src/main/java/org/example/Main.java
+++ Section14-LambdaExpressions-FunctionalInterface/src/main/java/org/example/Main.java
@@ -29,7 +29,31 @@ public class Main {
             }
         };
 
-        people.sort(comparatorLastName);
+        people.sort((o1, o2) -> o1.lastName().compareTo(o2.lastName()));
         System.out.println(people);
+
+        interface EnhancedComparator<T> extends Comparator<T> {
+            int secondLevel(T o1, T o2);
+        }
+
+        var comparatorMixed = new EnhancedComparator<Person>() {
+
+            @Override
+            public int compare(Person o1, Person o2) {
+                int result = o1.lastName().compareTo(o2.lastName());
+                return (result == 0 ? secondLevel(o1, o2) : result);
+            }
+
+            @Override
+            public int secondLevel(Person o1, Person o2) {
+                return o1.firstName().compareTo(o2.firstName());
+            }
+        };
+
+
+        people.sort(comparatorMixed);
+        System.out.println(people);
+
+
     }
 }
